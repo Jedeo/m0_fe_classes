01130c98192eb495c713bba339e4e8f0a3970106
Eric Weissman <eric@example.com>
2022-06-10 21:52:25 -0600

    Initial commit

--- oop.js
+++ oop.js
@@ -3,14 +3,48 @@
 // it should have a color property, that is silver by default
 // it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
 
+class Unicorn{
+  //greating/intializing an object in class "Unicorn"
+  constructor(color,height, speed){
+    this.color = "silver";
+    this.height = height;
+    this.speed = speed;
+  }
+  //method
+  say(passed_text){
+    this.passed_text = passed_text
+    console.log(`*~* ${passed_text} *~* `);
+  }
+}
 
+var unicorn_object = new Unicorn();
+console.log(unicorn_object);
+//calling and passing string to "say method"
+//unicorn_object.say("yellow");
 
 // Write a class called Vampire
 // it should have a dynamic name property
 // it should have a pet property, that is a bat, by default BUT it could be dynamic if info is passed in initially
 // it should have a thirsty property, that is true by default
 // it should have a drink method. When called, the thirsty property changes to false
+class Vampire{
+  constructor(good_bad, hair_color, height){
+    this.pet ="Bat";
+    this.good_bad = good_bad;
+    this.hair_color = hair_color;
+    this.thirsty = true;
+  }
+  drink(pet){
+    this.thirsty = false;
+    this.pet = pet
+  }
+}
 
+var vampire_info = new Vampire("Good", "black", 12);
+console.log(vampire_info);
+
+vampire_info.drink("dog");
+console.log(vampire_info);
 
 
 //  Write a Dragon class
@@ -20,7 +54,25 @@
 //  it should have a isHungry property that is true by default
 //  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
   // HINT: This requires you to use your knowledge from the conditionals lesson
+class Dragon{
+  constructor(name, rider, color){
+    this.isHungry = true;
+    this.name = name;
+    this.rider = rider;
+    this.color = color;
+  }
+  eat_food(amountOf_food){
+    if(amountOf_food >= 4){
+      this.isHungry = false
+    }
+  }
+}
 
+var dragon_info = new Dragon("jj", "jedeo", "Brown");
+console.log(dragon_info);
+
+dragon_info.eat_food(5);
+console.log(dragon_info);
 
 
 //  BIG CHALLNEGE: Write a Hobbit class
@@ -31,3 +83,44 @@
 //  it should have an isAdult property (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 //  it should have an isOld property that defaults to false. once a Hobbit is 101, it is old.
 //  it should have a hasRing property. If the Hobbit's name is "Frodo", true, if not, false.
+
+class Hobbit{
+  constructor(name, disposition){
+    this.age = 0;
+    this.name = name;
+    this.disposition = disposition;
+    this.isAdult = false;
+    this.isOld = false;
+    this.hasRing = false;
+
+    if (name === "Frodo"){
+      this.hasRing = true;
+    }
+  } // end constructor
+  celebrateBirthday(){
+      this.age = this.age + 1;
+
+      if(this.age >= 33){
+        this.isAdult = true;
+      }
+      if (this.age  >= 101){
+        this.isOld = true;
+      }
+
+  }
+
+} //end of class
+
+var hobbit_info = new Hobbit("Frodo", "temper");
+console.log(hobbit_info);
+for (var i = 0; i < 102; i++){
+  hobbit_info.celebrateBirthday();
+  console.log(hobbit_info);
+}
+
+var hobbit_info2 = new Hobbit("jed", "chill");
+console.log(hobbit_info2);
+for (var i = 0; i < 102; i++){
+  hobbit_info2.celebrateBirthday();
+  console.log(hobbit_info2);
+}
--- self-eval.md
+++ self-eval.md
@@ -1,5 +1,8 @@
 # Self Evaluation
 
 - This final CFU encompassed everything you've learned in Mod 0, and maybe even required some learning outside of what you've explicitly been taught. How did it feel as you faced these challenges?
+-this by far was the must challenging assignment I worked on it took me pretty much all day but i think I have a better understanding of classes now
 - How is your workflow going, in terms of taking breaks, staying focused, etc.?
+- I have trying to do 10 min breaks every 2 hours. staying focused is a little challenging because I want to help people who may need it so i tend to look at my messages more than I should.
 - What do you need to do to be ready for intense technical lessons and challenges like this, for 6 hours/day next week? What support do you need?
+-continue studying what i have learned so far.
